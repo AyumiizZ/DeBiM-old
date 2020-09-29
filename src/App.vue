a1a2ecaf247f3b95e2df77f41acaa638a2b02d60
AyumiizZ <Korrawitc@gmail.com>
2020-09-30 01:53:09 +0700

    :sparkles: add echarts

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
   <div id="app">
-    <!-- <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button> -->
-    <!-- <font-awesome-icon icon="user-secret" /> -->
     <NavBar />
     <SideBar />
     <main id="page-wrap">
-      <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-      <!-- <NavBar /> -->
       <router-view />
     </main>
   </div>
@@ -38,8 +31,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-.home {
-  background-color: white;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="8">col-sm-8</b-col>
+        <b-col sm="4">col-sm-4</b-col>
+      </b-row>
+
+      <b-row>
+        <b-col sm>col-sm</b-col>
+        <b-col sm>col-sm</b-col>
+        <b-col sm>col-sm</b-col>
+      </b-row>
+    </b-container>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style scoped>
+.home {
+  background-color: white;
+}
+</style>
